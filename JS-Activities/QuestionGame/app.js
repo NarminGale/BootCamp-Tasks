4b15684fa8d8dc95b49616ae50b9dd6cc2051938
NarminGale <narmingale@gmail.com>
2021-04-09 18:51:29 +0400

    Modified some codes

--- JS-Activities/QuestionGame/app.js
+++ JS-Activities/QuestionGame/app.js
@@ -2,28 +2,23 @@ let questions = {
   q1: ['What is my name?', 'narmin'],
   q2: ['What is my surname?', 'aliyeva'],
   q3: ['Do I like red?', 'yes'],
-  //   q4: ['What is my speciality?', 'cs'],
-  //   q5: ['How old I am?', '20'],
-  //   q6: ['What is birthday?', '30'],
-  //   q7: ['Who is best friend?', 'fidan'],
-  //   q8: ['What is favorite music?', 'retro'],
-  //   q9: ['Where do I want to study master ?', 'america'],
-  //   q10: ['Who is my teacher?', 'elshad'],
+  q4: ['What is my speciality?', 'computer science'],
+  q5: ['How old I am?', '20'],
 }
-
 function askQuestion() {
   let userScore = 0
+  let question = Object.values(questions)
   let lengthOfQuestions = Object.keys(questions).length
   for (let i = 0; i < lengthOfQuestions; i++) {
-    let value = Object.values(questions)[i][0]
+    let value = question[i][0]
     let answer = prompt(value)
-    if (answer === Object.values(questions)[i][1]) {
+    if (answer === question[i][1]) {
       alert('You are right')
       userScore++
     } else {
       alert('You are wrong')
     }
   }
-  console.log(`Your Score is ${userScore}`)
+  alert(`Your Score is ${userScore}`)
 }
 askQuestion()
