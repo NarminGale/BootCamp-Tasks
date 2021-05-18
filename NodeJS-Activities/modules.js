94095f51978c88093748a53161628f1dbdbe63be
NarminGale <narmingale@gmail.com>
2021-05-18 21:35:25 +0400

    Completed task

---
+++ NodeJS-Activities/modules.js
@@ -0,0 +1,11 @@
+let arr = process.argv
+let num = parseInt(arr[2])
+let divider = parseInt(arr[3])
+
+sum = 0
+let i = 0
+for (i; i < num; i += divider) {
+  sum += i
+  console.log(i)
+}
+console.log(sum)
