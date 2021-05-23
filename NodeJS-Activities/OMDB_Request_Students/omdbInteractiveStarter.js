0955a3655dd270a97c2c3dd742468fdfc248eafb
NarminGale <narmingale@gmail.com>
2021-05-23 14:05:48 +0400

    Completed OMDB_Request_Students task

---
+++ NodeJS-Activities/OMDB_Request_Students/omdbInteractiveStarter.js
@@ -0,0 +1,23 @@
+// INSTRUCTIONS:
+// ---------------------------------------------------------------------------------------------------------
+// Level 1:
+// Take any movie with a word title (ex: Cinderella) as a Node argument and retrieve the year it was created
+
+// Level 2 (More Challenging):
+// Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrieve the year it was created.
+// ---------------------------------------------------------------------------------------------------------
+
+// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+// ...
+const request = require('request')
+
+// Grab or assemble the movie name and store it in a variable called "movieName"
+var movieName = process.argv[2]
+
+var queryUrl =
+  'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy'
+
+request(queryUrl, function (err, res, body) {
+  let json = JSON.parse(body)
+  console.log(json.Writer)
+})
